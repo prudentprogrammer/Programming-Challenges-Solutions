83070a7971b85caf45af179381c6dfc66e4194ba
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-09-16 23:49:52 -0700

    Reverted tex quotes file

--- uvaProblems/272 Tex Quotes.py
+++ uvaProblems/272 Tex Quotes.py
@@ -1,6 +1,13 @@
-# https://uva.onlinejudge.org/external/11/1124.pdf
+# Link: https://uva.onlinejudge.org/external/2/272.pdf
 
 from sys import stdin
 
+endQuote = False
+brackets = ['``', "''"]
 for line in stdin:
-  print(line.strip())
+  for letter in line:
+    if letter == '"':
+      print(brackets[endQuote], end='')
+      endQuote = 1 - endQuote
+    else:
+      print(letter, end='')
