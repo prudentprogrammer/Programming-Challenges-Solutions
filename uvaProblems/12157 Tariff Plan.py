c1ee637c2dd4f6c0fb7b1f1cdecf3ec44e877ce8
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-09-26 23:06:14 -0700

    Adding tariff plan

--- uvaProblems/12157 Tariff Plan.py
+++ uvaProblems/12157 Tariff Plan.py
@@ -1,5 +1,15 @@
-# Link: 
+# Link: https://uva.onlinejudge.org/external/121/12157.pdf
 
-for _ in range(int(input())):
-  pass
-  
+for counter in range(1, int(input()) + 1):
+  numberOfCalls = input()
+  callDurations = [int(x) for x in input().split()]
+  totalMiles = totalJuices = 0
+  for call in callDurations:
+    totalMiles += (call // 30) * 10 + 10
+    totalJuices += (call // 60) * 15 + 15
+  if totalMiles < totalJuices:
+    print('Case {}: Mile {}'.format(counter, totalMiles))
+  elif totalJuices < totalMiles:
+    print('Case {}: Juice {}'.format(counter, totalJuices))
+  else:
+    print('Case {}: Mile Juice {}'.format(counter, totalMiles))
