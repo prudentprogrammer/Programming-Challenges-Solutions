c8e25cb1db337967a9c0bc7492d1eefb4042b70a
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-09-26 20:28:01 -0700

    Adding Google problem

---
+++ uvaProblems/10919 Prerequisites.py
@@ -0,0 +1,5 @@
+# Link: 
+
+for _ in range(int(input())):
+  pass
+  
---
+++ uvaProblems/12015 Google is Feeling Lucky.py
@@ -0,0 +1,17 @@
+# Link: https://uva.onlinejudge.org/external/120/12015.pdf
+
+import collections
+
+for counter in range(1, int(input()) + 1):
+  webpagesMap = collections.defaultdict(list)
+  mostRelevantWebpage = 0
+
+  for _ in range(10):
+    webpage, relevance = input().split()
+    relevance = int(relevance)
+    webpagesMap[relevance].append(webpage)
+    mostRelevantWebpage = max(mostRelevantWebpage, int(relevance))
+
+  print('Case #{}:'.format(counter))
+  for webpage in webpagesMap[mostRelevantWebpage]:
+    print(webpage)
