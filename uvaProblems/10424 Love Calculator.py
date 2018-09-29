0e95ba2979f69ae40cd4753bf55daf2666169258
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-09-29 00:14:27 -0700

    Finished Love Calculator

--- uvaProblems/10424 Love Calculator.py
+++ uvaProblems/10424 Love Calculator.py
@@ -2,17 +2,22 @@
 from string import ascii_lowercase
 
 def getSum(name):
+  if name < 10:
+    return name
 
-  while len(name) != 1:
-
-
+  curSum = 0
+  while name != 0:
+    curSum += (name % 10)
+    name //= 10
+  return getSum(curSum)
 
 while True:
   try:
     person1, person2 = input(), input()
     letterDict = { letter: ind for ind, letter in enumerate(ascii_lowercase, 1) }
-    print(sum([letterDict.get(letter, 0) for letter in person1]))
-    print(sum([letterDict.get(letter, 0) for letter in person2]))
+    person1Sum = getSum(sum([letterDict.get(letter.lower(), 0) for letter in person1]))
+    person2Sum = getSum(sum([letterDict.get(letter.lower(), 0) for letter in person2]))
+    print('{:.2f} %'.format( (min(person1Sum, person2Sum) * 100.0)/ max(person1Sum, person2Sum)))
 
   except EOFError:
     break
