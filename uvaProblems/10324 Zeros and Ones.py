4f30a074037947f4d5bb33946335818e3d05bfde
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-09-28 23:50:30 -0700

    Adding love calculator problem

--- uvaProblems/10324 Zeros and Ones.py
+++ uvaProblems/10324 Zeros and Ones.py
@@ -1,5 +1,25 @@
-# Link: 
+# Link: https://uva.onlinejudge.org/external/103/10324.pdf
 
-for _ in range(int(input())):
-  pass
-  
+totalTestCases = 1
+while True:
+  try:
+    currLine = input()
+    if currLine == '\n':
+      break
+
+    queries = int(input())
+    totalSum = 0
+    print('Case {}:'.format(totalTestCases))
+    for query in range(queries):
+      i, j = [int(x) for x in input().split()]
+      i, j = min(i, j), max(i, j)
+      if (currLine[j] - currLine[i]) in [0, j - i + 1]:
+          print('No')
+          break
+      else: # Using the quirky else syntax for python
+        print('Yes')
+
+    totalTestCases += 1
+
+  except EOFError: # Don't know how many lines in advance
+    break
--- uvaProblems/10424 Love Calculator.py
+++ uvaProblems/10424 Love Calculator.py
@@ -1,5 +1,21 @@
-# Link: 
+# Link: https://uva.onlinejudge.org/external/104/10424.pdf
+from string import ascii_lowercase
 
-for _ in range(int(input())):
-  pass
+def getSum(name):
+
+  while len(name) != 1:
+
+
+
+while True:
+  try:
+    person1, person2 = input(), input()
+    letterDict = { letter: ind for ind, letter in enumerate(ascii_lowercase, 1) }
+    print(sum([letterDict.get(letter, 0) for letter in person1]))
+    print(sum([letterDict.get(letter, 0) for letter in person2]))
+
+  except EOFError:
+    break
+
+
   
