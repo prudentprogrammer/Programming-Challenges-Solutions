00cfb296eed5f3e99ad29594477843465094e213
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-10-13 00:09:10 -0700

    Adding expert enough problem

--- uvaProblems/1237 Expert Enough.py
+++ uvaProblems/1237 Expert Enough.py
@@ -1,6 +1,7 @@
 # Link: https://uva.onlinejudge.org/external/12/1237.pdf
 
-for _ in range(int(input())):
+totalTestCases = int(input())
+for testCases in range(totalTestCases):
   size_of_db = int(input())
   data = []
   for _ in range(size_of_db):
@@ -11,11 +12,21 @@ for _ in range(int(input())):
   for _ in range(int(input())): # queries
     price = int(input())
     count = 0
+    greaterThan2 = False
+    currentMake = ''
     for m, l, h in data:
-      if l < price < h:
+      if l <= price <= h:
         count += 1
         if count >= 2:
           print('UNDETERMINED')
+          greaterThan2 = True
           break
+        currentMake = m
+
+    if count == 0 and not greaterThan2:
+      print('UNDETERMINED')
     if count == 1:
-      print()
+      print(currentMake)
+
+  if testCases != totalTestCases - 1:
+    print()
