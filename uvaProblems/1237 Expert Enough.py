f05562618d33de5648eb5af17c94c802478002b5
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-10-12 23:58:28 -0700

    Adding expert enough problem

---
+++ uvaProblems/1237 Expert Enough.py
@@ -0,0 +1,21 @@
+# Link: https://uva.onlinejudge.org/external/12/1237.pdf
+
+for _ in range(int(input())):
+  size_of_db = int(input())
+  data = []
+  for _ in range(size_of_db):
+    currLine = input().split()
+    make, low, high = currLine[0], int(currLine[1]), int(currLine[2])
+    data.append((make, low, high))
+
+  for _ in range(int(input())): # queries
+    price = int(input())
+    count = 0
+    for m, l, h in data:
+      if l < price < h:
+        count += 1
+        if count >= 2:
+          print('UNDETERMINED')
+          break
+    if count == 1:
+      print()
