a37e1024164fd098dfde85b90b17accdd98b1afa
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-12-16 10:35:23 -0800

    Clean up files

--- Codeforces/Education_Codeforces_Round_1/C_Nearest_Vectors.py
+++ Codeforces/Education_Codeforces_Round_1/C_Nearest_Vectors.py
@@ -6,23 +6,24 @@ for i in range(int(input())):
   vectors.append((atan2(y, x), i + 1))
 
 vectors.sort()
-min_diff = float('+inf')
+min_diff = float('+inf') 
 first_ind = second_ind = -1
+n = len(vectors)
 
-print(vectors)
+# print(vectors)
 
 # The key here is to compare the difference between ith
 # and i + 1-th vector and take the minimum. We should also
 # take the difference between the last vector and the very first one
 # in the very end.
-for i in range(len(vectors)):
-  first_vector, second_vector = vectors[(i + 1) % len(vectors)] , vectors[i]
+for i in range(n - 1):
+  first_vector, second_vector = vectors[(i + 1) % n] , vectors[i]
   diff = first_vector[0] - second_vector[0]
-  print(diff)
+  # print(diff)
   if diff < 0:
     diff += 2 * pi
 
-  if diff < min_diff:
+  if diff <= min_diff:
     min_diff = diff
     first_ind, second_ind = first_vector[1], second_vector[1] 
 
