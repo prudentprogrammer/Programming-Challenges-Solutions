2252e6d8e834f8489ea1de33f5acda050eefcb29
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-01-13 13:06:52 -0800

    Create 11729 Commando War.py

---
+++ uvaProblems/11729 Commando War.py
@@ -0,0 +1,23 @@
+# Link: https://uva.onlinejudge.org/external/117/11729.pdf
+
+def get_minimum_time(soldiers):
+    res = total_brief_times = 0
+    for brief_time, execution_time in soldiers:
+        total_brief_times += brief_time
+        res = max(res, total_brief_times + execution_time)
+    return res
+
+counter = 0
+while True:
+    test_cases = input()
+    if test_cases == "0":
+        break
+    soldiers = []
+    
+    for i in range(int(test_cases)):
+        brief_time, execution_time = [int(x) for x in input().split()]
+        soldiers.append((brief_time, execution_time))
+
+    soldiers.sort(key=lambda x: -x[1]) # Sort by descending time of execution time.
+    print('Case {}: {}'.format(counter + 1, get_minimum_time(soldiers)))
+    counter += 1
