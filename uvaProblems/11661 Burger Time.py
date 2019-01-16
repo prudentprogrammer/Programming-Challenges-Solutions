7d169fc5a19ea3f832881f3ed072d02753e03b4e
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-01-15 21:25:23 -0800

    Update 11661 Burger Time.py

--- uvaProblems/11661 Burger Time.py
+++ uvaProblems/11661 Burger Time.py
@@ -1,5 +1,21 @@
-# Link: 
+# Link: https://uva.onlinejudge.org/external/116/11661.pdf
 
-for _ in range(int(input())):
-  pass
-  
+def get_min_distance(highway):
+    prev = 0
+    minDistance = len(highway)
+    for ind, space in enumerate(highway):
+        if space == 'Z':
+            return 0
+        elif space in ['D', 'R']:
+            if highway[prev] != '.' and space != highway[prev]:
+                minDistance = min(minDistance, ind - prev)
+            prev = ind
+    
+    return minDistance
+
+while True:
+    test_cases = input()
+    if test_cases == "0":
+        break
+    highway = input()
+    print(get_min_distance(highway))
