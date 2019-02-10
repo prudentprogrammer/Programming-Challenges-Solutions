0228a9cba024e36c8761c33510d17c902b65d6f6
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-02-09 23:14:06 -0800

    Update 00661 Blowing Fuses.py

--- uvaProblems/00661 Blowing Fuses.py
+++ uvaProblems/00661 Blowing Fuses.py
@@ -1,5 +1,20 @@
-# Link: 
+# Link: https://uva.onlinejudge.org/external/6/661.pdf
 
-for _ in range(int(input())):
-  pass
-  
+total_fuses = 21
+
+fuses = [0] * total_fuses
+while True:
+    curr_line = input()
+    if curr_line == '0 0 0':
+        break # End of input
+    
+    devices, ops, capacity = [int(x) for x in curr_line.split()]
+    
+    devices_capacity = [int(input()) for _ in range(devices)]
+    for ind, capacity in enumerate(devices_capacity, 1):
+        fuses[ind] = capacity
+
+    for i in range(ops):
+        curr_outlet = int(input())
+        if not fuses[curr_outlet]: # Then toggle it
+            fuses[curr_outlet] 
