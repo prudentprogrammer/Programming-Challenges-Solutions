8a390a7db7223091bb3b44b17720c1d229e4b0c8
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-02-13 19:39:08 -0800

    Update 10189 Minesweeper.py

--- uvaProblems/10189 Minesweeper.py
+++ uvaProblems/10189 Minesweeper.py
@@ -1,7 +1,7 @@
 # Link: https://uva.onlinejudge.org/external/101/10189.pdf
 
 def print_revealed_grid(grid, rows, cols):
-    deltas = [(x, y) for x in range(-1, 2) for y in range(-1, 2) if [x, y] != [0, 0]]    
+    deltas = [(x, y) for x in range(-1, 2) for y in range(-1, 2) if [x, y] != [0, 0]]
     for row in range(rows):
         curRow = []
         for col in range(cols):
@@ -16,14 +16,22 @@ def print_revealed_grid(grid, rows, cols):
                     elif grid[nextX][nextY] == '*':
                         count += 1
                 curRow.append(str(count))
-        print(curRow)
-    print()
+        print(''.join(curRow))
 
-
+curr_test_case = 1
 while True:
-    rows, cols = [int(x) for x in input().split()]
-    print(rows, cols)
+    currLine = input()
+    if len(currLine) == 0:
+        continue # Skip the empty line
+    rows, cols = [int(x) for x in currLine.split()]
+    #print(rows, cols)
     if [rows, cols] == [0, 0]:
         break # end of input
+
     grid = [input() for _ in range(rows)]
+    if curr_test_case > 1:
+        print() # New line after every case
+
+    print('Field #{}:'.format(curr_test_case))
     print_revealed_grid(grid, rows, cols)
+    curr_test_case += 1
