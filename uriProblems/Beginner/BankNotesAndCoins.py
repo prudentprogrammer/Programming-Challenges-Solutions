e81eb8d4cb1e10fea507dec5903fff7552388f18
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-07-03 10:55:03 -0700

    Adding banknotes problem

--- uriProblems/Beginner/BankNotesAndCoins.py
+++ uriProblems/Beginner/BankNotesAndCoins.py
@@ -1,7 +1,20 @@
-amount = int(input().strip())
-print(amount)
-for denom in [100, 50, 20, 10, 5, 2, 1]:
-    count = int(amount / denom) # 76 / 50
-    print('{} nota(s) de R$ {},00'.format(count, denom))
+def printNotes(amount, heading, units, denominations):
+  firstTime = True
+  for denom in denominations:
+    count = int(amount / denom)
+    if firstTime:
+        print(heading)
+        firstTime = False
+
+    print('{} {} de R$ {:.2f}'.format(count, units, denom / 100.0 if heading == 'MOEDAS:' else denom))
     if count > 0:
         amount = (amount % denom)
+
+  return amount
+
+amount = input().strip().split('.')
+dollarAmount, coinAmount = int(amount[0]), int(amount[1])
+remaining = printNotes(dollarAmount, 'NOTAS:', 'nota(s)', [100, 50, 20, 10, 5, 2])
+if remaining > 0:
+  coinAmount = remaining * 100 + coinAmount
+printNotes(coinAmount, 'MOEDAS:', 'moeda(s)', [100, 50, 25, 10, 5, 1])
