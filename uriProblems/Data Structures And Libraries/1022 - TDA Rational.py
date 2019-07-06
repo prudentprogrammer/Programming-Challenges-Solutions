735a2ebc855881fd036bdca4bff60647ce4a8e18
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-07-06 00:04:46 -0700

    Removed old folder

--- uriProblems/Data Structures And Libraries/1022 - TDA Rational.py
+++ uriProblems/Data Structures And Libraries/1022 - TDA Rational.py
@@ -1,2 +1,14 @@
-info = [int(x) for x in input().split()]
-print(info)
+# Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1022
+
+for _ in range(int(input())):
+  expression = input().split()
+  N1, D1 = int(expression[0]), int(expression[2])
+  N2, D2 = int(expression[4]), int(expression[6])
+  if expression[3] == '+':
+    print('{}/{}'.format(N1*D2 + N2*D1, D1*D2))
+  elif expression[3] == '-':
+    print()
+  elif expression[3] == '*':
+    print()
+  elif expression[3] == '/':
+    print()
