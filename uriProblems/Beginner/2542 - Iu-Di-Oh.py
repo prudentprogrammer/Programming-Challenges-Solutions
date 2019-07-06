27b076aef2005e493eb843aa8c2873ce10ffe558
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-07-05 23:36:09 -0700

    Adding all problems

---
+++ uriProblems/Beginner/2542 - Iu-Di-Oh.py
@@ -0,0 +1,2 @@
+info = [int(x) for x in input().split()]
+print(info)
--- uriProblems/create_folders.py
+++ uriProblems/create_folders.py
@@ -2,24 +2,24 @@ import sys
 import os
 import pickle
 import string
+import shutil
 
 parent_dir = r'/Users/arjunbharadwaj/Documents/Programming-Challenges-Solutions/uriProblems'
 with open('meta_data', 'rb') as handle:
   file_categories = pickle.load(handle)
   for category, problems in file_categories.items():
-    problems = [s.replace('!','') for s in problems]
-    if not os.path.exists(category):
+    print(category)
+    problems = [s.replace('!','').replace(r'/','') for s in problems]
+    if os.path.exists(category):
+      shutil.rmtree(category)
+    else:
       os.mkdir(category)
       os.chdir(category)
       for problem in problems:
         f = open(problem, 'w+')  # open file in append mode
-        source_code = '''
-        info = [int(x) for x in input().split()]
-        print(info)
-        '''
+        source_code = 'info = [int(x) for x in input().split()]\nprint(info)'
         f.write(source_code)
         f.close()
-    
     os.chdir(parent_dir)
 
 
