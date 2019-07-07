9ee27236f969c8e142ae312e32ac1f7496d4b51d
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-07-07 14:19:42 -0700

    Adding solved problems

--- uriProblems/Ad-Hoc/1030 - Flavious Josephus Legend.py
+++ uriProblems/Ad-Hoc/1030 - Flavious Josephus Legend.py
@@ -1,2 +1,12 @@
-info = [int(x) for x in input().split()]
-print(info)
+st = []
+def josephus(N, K):
+  if N == 1:
+    return 1
+  else:
+    st.append((N - 1))
+    return ((josephus(N - 1, K) + K - 1) % N) + 1
+
+test_cases = int(input())
+for i in range(1, test_cases+1):
+  total, start = [int(x) for x in input().split()]
+  print('Case {}: {}'.format(i, josephus(total, start)))
