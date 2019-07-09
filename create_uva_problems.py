87a186d20eeb335ab6aeb1995d7119a568bde57a
Arjun Bharadwaj <bhararj@amazon.com>
2019-07-08 19:54:56 -0700

    Add create script

--- create_uva_problems.py
+++ create_uva_problems.py
@@ -12,23 +12,25 @@ r = requests.get(URL)
 
 # extracting data in json format
 problem_objs = r.json()
-res = []
+res = {'Python': {}, 'Java': {}}
 for problem in problem_objs:
-    res.append('{} - {}.py'.format(problem[1], problem[2]))
+  res['Python'].append('{} - {}.py'.format(problem[1], problem[2]))
+  res['Java'].append('{} - {}.java'.format(problem[1], problem[2]))
 
 parent_dir = os.getcwd()
-folder_name = os.path.join(parent_dir, 'UVA Problems')
-if os.path.exists(folder_name):
-  shutil.rmtree(folder_name)
-else:
-  os.mkdir(folder_name)
+for ind, sol_type in enumerate(['Python Solutions', 'Java Solutions']):
+  folder_name = os.path.join(parent_dir, 'UVA Problems (Masterset)/{}'.format(sol_type))
+  if os.path.exists(folder_name):
+    shutil.rmtree(folder_name)
+  else:
+    os.mkdir(folder_name)
 
-os.chdir(folder_name)
-
-for filename in res:
+  os.chdir(folder_name)
+  curr_key_folder_type = 'Python' if ind == 0 else 'Java'
+  for filename in res[curr_key_folder_type]:
     filename = filename.replace('!','').replace(r'/','')
     f = open(filename, 'w+')  # open file in append mode
     source_code = '# Link: \n# Solution is yet to be filled and updated\nfor _ in range(int(input())):\n  pass'
     f.write(source_code)
     f.close()
-os.chdir(parent_dir)
+    os.chdir(parent_dir)
