12bb042479c8d78ed154b8410ec95097ae148e33
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2019-08-03 01:34:16 +0000

    Adding going to market prob

--- uriProblems/Data Structures And Libraries/1281 - Going to the Market.py
+++ uriProblems/Data Structures And Libraries/1281 - Going to the Market.py
@@ -1,2 +1,4 @@
-info = [int(x) for x in input().split()]
-print(info)
+for _ in range(int(input())):
+  sale_dict = {x[0]: float(x[1]) for x in [input().split() for _ in range(int(input()))]}
+  total = sum([float(x[1]) * sale_dict[x[0]] for x in [input().split() for _ in range(int(input()))]])
+  print('R$ {:.2f}'.format(total))
